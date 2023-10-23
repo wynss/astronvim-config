a2bae8ac18dbeacf557c4ba2dd28927a785b4753
wynss <toniaxelsson@gmail.com>
2023-10-23 09:27:29 +0200

    add LSPs and TODO plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,13 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope"}
+    }
+  }
 }
